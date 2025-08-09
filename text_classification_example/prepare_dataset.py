45c17d5124e87fd82c534d6004dd6e3973698821
u-masao <4973920+u-masao@users.noreply.github.com>
2025-08-09 12:08:36 +0900

    分類機能を追加

--- text_classification_example/prepare_dataset.py
+++ text_classification_example/prepare_dataset.py
@@ -1,7 +1,10 @@
+import json
 from pathlib import Path
+import time
 
 from loguru import logger
 import mlflow
+import numpy as np
 from object_cache import object_cache
 import ollama
 import pandas as pd
@@ -39,22 +42,60 @@ class CriteriaClasses(BaseModel):
 
 
 class ModelResponseStructure(BaseModel):
+    criterias: CriteriaClasses
     summary_text: str = Field(
         ...,
-        description="要約（日本語）",
+        description="日本語で要約",
     )
-    criterias: CriteriaClasses
 
 
 @object_cache
-def generate_response(text: str, model_name: str = "gemma3:4b", num_ctx: int = 1024 * 16) -> str:
-    prompt = f"次の文章を日本語で要約して評価して。\n\n{text}"
-    return ollama.generate(
-        model=model_name,
-        prompt=prompt,
-        options={"temperature": 0, "num_ctx": num_ctx},
-        format=ModelResponseStructure.model_json_schema(),
-    ).response
+def generate_response_ollama(
+    text: str,
+    model_name: str,
+    num_ctx: int = 128,
+    max_retry: int = 5,
+    temperature_delta: float = 0.01,
+):
+    return {}
+    user_prompt = f"""
+        評価規定に従い次のテキストを評価して。
+        必ずすべての項目を評価して。
+
+        ```
+        {text}
+        ```
+
+        繰り返すが絶対に日本語で要約すること。
+        テキスト本文が英語であっても日本語で要約すること。
+        """
+    try_count: int = 0
+    temperature: float = 0.0
+    while try_count < max_retry:
+        try:
+            response = ollama.generate(
+                model=model_name,
+                prompt=user_prompt,
+                options={"temperature": temperature, "num_ctx": num_ctx},
+                format=ModelResponseStructure.model_json_schema(),
+            ).response
+            logger.info(f"response: {response}")
+            response_dict = json.loads(response)
+            return response_dict
+        except ollama.ResponseError as e:
+            logger.info(f"Ollama API エラー: {e}")
+        except json.JSONDecodeError:
+            logger.info("JSON Decode エラー")
+        except Exception as e:
+            logger.info(f"予期しないエラー: {e}")
+            raise e
+
+        logger.info(f"リトライ中: {try_count + 1} / {max_retry}")
+        temperature += temperature_delta
+        try_count += 1
+        time.sleep(1)
+    logger.warning(f"{max_retry} 回リクエストしましたがすべて失敗でした")
+    return {}
 
 
 class Args(Tap):
@@ -65,8 +106,19 @@ class Args(Tap):
     output_valid_filepath: Path
     output_test_filepath: Path
 
+    # split
+    split_random_seed: int = 1234
+    train_ratio: float = 0.6
+    valid_ratio: float = 0.2
+    test_ratio: float = 0.2
+
     # experiment managemento definition
     mlflow_run_name: str | None = None
+
+    # ollama_model_name
+    # ollama_model_name: str = "gemma3:4b"
+    ollama_model_name: str = "gemma3:1b"
+    ollama_num_ctx: int = 1024 * 31
 
     def configure(self):
         # input definition
@@ -83,26 +135,67 @@ class Experiment:
         # load dataset
         input_df = pd.read_parquet(self.args.input_filepath)
 
+        input_df = input_df.head(100)
+
         # log input
         print(input_df)
 
+        # init result
+        result_df = input_df.copy()
+
         # 分類
-        tqdm.pandas()
-        input_df["generated"] = input_df["text"].progress_apply(generate_response)
+        tqdm.pandas(desc="処理中")
+        generated_sr = input_df["text"].progress_apply(
+            lambda x: generate_response_ollama(
+                x,
+                model_name=self.args.ollama_model_name,
+                num_ctx=self.args.ollama_num_ctx,
+            )
+        )
 
-        input_df.to_parquet("data/interim/temp.parquet")
+        # デバッグ用にテンポラリファイルを出力
+        result_df.to_parquet("data/interim/temp.parquet")
 
-        # split data
-        result_df = {
-            "train": input_df.iloc[:2000],
-            "valid": input_df.iloc[2000:3000],
-            "test": input_df.iloc[3000:],
-        }
+        # 分類結果を横に結合
+        result_df = pd.concat([result_df, pd.json_normalize(generated_sr)], axis=1)
+
+        # データ分割
+        train_df, valid_df, test_df = self._split_dataframe(
+            result_df,
+            train_ratio=self.args.dataset.train_ratio,
+            valid_ratio=self.args.dataset.valid_ratio,
+            test_ratio=self.args.dataset.test_ratio,
+            random_seed=self.args.dataset.split_random_seed,
+        )
 
         # output dataset
-        result_df["train"].to_parquet(self.args.output_train_filepath)
-        result_df["valid"].to_parquet(self.args.output_valid_filepath)
-        result_df["test"].to_parquet(self.args.output_test_filepath)
+        train_df.to_parquet(self.args.output_train_filepath)
+        valid_df.to_parquet(self.args.output_valid_filepath)
+        test_df.to_parquet(self.args.output_test_filepath)
+
+        # ログ出力
+        mlflow.log_metrics(
+            {
+                "length.train": len(train_df),
+                "length.valid": len(valid_df),
+                "length.test": len(test_df),
+            }
+        )
+
+    def _split_dataframe(
+        self,
+        df: pd.DataFrame,
+        train_ratio: float = 0.6,
+        valid_ratio: float = 0.2,
+        test_ratio: float = 0.2,
+        random_seed: int = 1234,
+    ):
+        total_ratio = train_ratio + valid_ratio + test_ratio
+        train_ratio = train_ratio / total_ratio
+        valid_ratio = valid_ratio / total_ratio
+        test_ratio = test_ratio / total_ratio
+        split_points = [int(len(df) * train_ratio), int(len(df) * (train_ratio + valid_ratio))]
+        return np.split(df.sample(frac=1, random_state=random_seed), split_points)
 
 
 def main(args: Args) -> None:
